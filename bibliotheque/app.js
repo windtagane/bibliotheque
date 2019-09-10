ce7a122ca641bf824182b251d98824c89de4ad41
ithaqua974 <tristan.hoarau@gmail.com>
2019-09-10 15:29:04 +0400

    body-parser

--- bibliotheque/app.js
+++ bibliotheque/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 var indexRouter = require('./routes/index');
